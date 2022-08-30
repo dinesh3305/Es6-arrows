97abc7876381fad20554f792c6ede5a4fecaab28
dinesh <bogidany3305@gmail.com>
2022-08-30 19:22:02 +0530

    no changes

---
+++ arrow.js
@@ -0,0 +1,56 @@
+// let str=(x)=>x*3;
+// console.log(str(10));//arrow function
+
+// let x=function(x,y){
+    // return x*y;
+// }
+// console.log(x(33,2))//multiply function
+
+
+// let x=(x,y)=>x*y;
+// console.log(x(33,2))
+
+
+// function myfunction(p1,p2){
+//     return(p1*p2);
+// }
+// console.log(myfunction)(10,9);//normal function
+
+//word length
+// let din=(str)=>str.length;
+// console.log(din('dinesh'));
+
+
+// const numbers=[5,5,5,6,6];
+// const newarr=numbers.map(dinesh);
+
+// function dinesh(num){
+//     return (num *10);
+// }
+// console.log(newarr);// map function
+
+// const na=["dinesh","mahesh","harish","rajesh"];
+// const newarr=na.map(arrow);
+
+// function arrow(n){
+//     console.log('hello'+ " "+ n);
+// }
+// console.log(newarr);
+
+// const na=[1,2,3];
+// const ss=(na.map(dinesh));
+
+// function dinesh(num){
+// return (num*10);
+// }
+// console.log(ss);
+
+// function sum(...din) {
+//     let sum =2;
+//     for (let a of din) sum =sum + a;
+//     return sum;
+//   }
+  
+//   let x = sum(4, 9, 16, 25, 29, 100, 66,);
+//   console.log(x);
+
